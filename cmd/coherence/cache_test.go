9422ac12ed3286beb2e3b5c97ca488b0286e21e3
SimonRichardson <stickupkid@gmail.com>
2018-02-05 22:28:42 +0000

    Introduction of a flow

--- cmd/coherence/cache_test.go
+++ cmd/coherence/cache_test.go
@@ -1,25 +1,48 @@
 package main
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
-	"os"
+	"net/http"
 	"os/exec"
 	"sync"
 	"testing"
 	"time"
+
+	"github.com/SimonRichardson/coherence/pkg/api"
+	"github.com/pkg/errors"
 )
 
 func TestCluster(t *testing.T) {
-	nodes := generateCluster(t, 3)
+	nodes := generateCluster(t, 8080, 3)
 	defer func() {
 		nodes.Close()
 	}()
 
-	time.Sleep(time.Second * 6)
+	serverURL := nodes.Get(0).API
+	_, code, err := selectValue(serverURL, "key1", "field1")
+	if code != 404 {
+		dumpStdErr(nodes)
+		t.Fatalf("expected: 404, actual: %d \nerr: %s\n", code, err.Error())
+	}
 
-	fmt.Println("run")
+	code, err = insertValue(serverURL, "key1", "field1", "value1", 1)
+	if code != 200 {
+		dumpStdErr(nodes)
+		t.Fatalf("expected: 200, actual: %d \nerr: %s\n", code, err.Error())
+	}
+
+	value, code, err := selectValue(serverURL, "key1", "field1")
+	if code != 200 {
+		dumpStdErr(nodes)
+		t.Fatalf("expected: 200, actual: %d \nerr: %s\n", code, err.Error())
+	}
+
+	fmt.Println(value)
 }
 
 type Nodes struct {
@@ -27,10 +50,23 @@ type Nodes struct {
 	nodes []Node
 }
 
-func (n *Nodes) Append(node Node) {
+func NewNodes(amount int) *Nodes {
+	return &Nodes{
+		nodes: make([]Node, amount),
+	}
+}
+
+func (n *Nodes) Insert(index int, node Node) {
 	n.mutex.Lock()
-	n.nodes = append(n.nodes, node)
+	n.nodes[index] = node
 	n.mutex.Unlock()
+}
+
+func (n *Nodes) Get(index int) Node {
+	n.mutex.Lock()
+	defer n.mutex.Unlock()
+
+	return n.nodes[index]
 }
 
 func (n *Nodes) Close() {
@@ -43,6 +79,7 @@ func (n *Nodes) Close() {
 
 type Node struct {
 	API, ClusterAPI string
+	stdOut, stdErr  *bytes.Buffer
 	cancel          context.CancelFunc
 }
 
@@ -50,30 +87,53 @@ func (n Node) Close() {
 	n.cancel()
 }
 
-func generateCluster(t *testing.T, amount int) *Nodes {
-	nodes := new(Nodes)
+func (n Node) StdOut() string {
+	return n.stdOut.String()
+}
+
+func (n Node) StdErr() string {
+	return n.stdErr.String()
+}
+
+func dumpStdErr(n *Nodes) {
+	for k, v := range n.nodes {
+		fmt.Printf("OUT: %d %s\n", k, v.StdOut())
+		fmt.Printf("ERR: %d %s\n------------\n", k, v.StdErr())
+	}
+}
+
+func generateCluster(t *testing.T, port, amount int) *Nodes {
+	nodes := NewNodes(amount)
 	for i := 0; i < amount; i++ {
 		go func(i int) {
-			port := 9090 + i
 			args := []string{
 				"cache", "-debug",
-				fmt.Sprintf("-api=tcp://0.0.0.0:%d", port),
+				fmt.Sprintf("-api=tcp://0.0.0.0:%d", port+i),
 			}
+
+			clusterPort := (port + 100) + i
 
 			if i > 0 {
 				args = append(args,
-					fmt.Sprintf("-cluster=tcp://0.0.0.0:%d", 8080+i),
+					fmt.Sprintf("-cluster=tcp://0.0.0.0:%d", clusterPort),
 					fmt.Sprintf("-peer=0.0.0.0:%d", defaultClusterPort),
 				)
 			}
 
-			cmd := exec.Command("../../dist/coherence", args...)
-			cmd.Stderr = os.Stderr
-			cmd.Stdout = os.Stdout
+			var (
+				stdOut = new(bytes.Buffer)
+				stdErr = new(bytes.Buffer)
+			)
 
-			nodes.Append(Node{
+			cmd := exec.Command("../../dist/coherence", args...)
+			cmd.Stderr = stdOut
+			cmd.Stdout = stdErr
+
+			nodes.Insert(i, Node{
 				API:        fmt.Sprintf("http://0.0.0.0:%d", port),
-				ClusterAPI: fmt.Sprintf("http://0.0.0.0:%d", 8080+i),
+				ClusterAPI: fmt.Sprintf("http://0.0.0.0:%d", clusterPort),
+				stdOut:     stdOut,
+				stdErr:     stdErr,
 				cancel: func() {
 					if err := cmd.Process.Kill(); err != nil {
 						log.Fatal(err)
@@ -87,5 +147,96 @@ func generateCluster(t *testing.T, amount int) *Nodes {
 		}(i)
 	}
 
+	time.Sleep(time.Second * 2)
+
 	return nodes
+}
+
+func selectValue(url, key, field string) (string, int, error) {
+	req, err := http.NewRequest("GET", fmt.Sprintf("%s/cache/select?key=%s&field=%s", url, key, field), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer resp.Body.Close()
+
+	bytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if resp.StatusCode != 200 {
+		var s struct {
+			Description string `json:"description"`
+			Code        int    `json:"code"`
+		}
+		if err := json.Unmarshal(bytes, &s); err != nil {
+			log.Fatal(err)
+		}
+		return "", resp.StatusCode, errors.Errorf("description: %s, code: %d", s.Description, s.Code)
+	}
+
+	var s struct {
+		Records struct {
+			Field string `json:"field"`
+			Value string `json:"value"`
+			Score int    `json:"score"`
+		} `json:"records"`
+	}
+	if err := json.Unmarshal(bytes, &s); err != nil {
+		log.Fatal(err)
+	}
+
+	return s.Records.Value, resp.StatusCode, nil
+}
+
+func insertValue(url, key, field, value string, score int64) (int, error) {
+	b, err := json.Marshal(api.MembersInput{
+		Members: []api.FieldValueScore{
+			api.FieldValueScore{
+				Field: api.Field(field),
+				Value: []byte(value),
+				Score: score,
+			},
+		},
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	req, err := http.NewRequest("POST", fmt.Sprintf("%s/cache/insert?key=%s", url, key), bytes.NewReader(b))
+	if err != nil {
+		log.Fatal(err)
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer resp.Body.Close()
+
+	bytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if resp.StatusCode != 200 {
+		var s struct {
+			Description string `json:"description"`
+			Code        int    `json:"code"`
+		}
+		if err := json.Unmarshal(bytes, &s); err != nil {
+			log.Fatal(err)
+		}
+		return resp.StatusCode, errors.Errorf("description: %s, code: %d", s.Description, s.Code)
+	}
+
+	return resp.StatusCode, nil
 }
